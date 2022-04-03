5f7e3bb795a1bf586c7056c4ecd6bad6e356b122
adi gunawan <broken.wayan@gmail.com>
2022-04-03 16:10:44 +0800

    first init

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  content: ['index.html'],
+  theme: {
+    container: {
+      center: true,
+      padding: '16px'
+    },
+    extend: {
+      screens: {
+        '2xl': '1320px'
+      },
+      colors: {
+        primary: '#14b8a6',
+        secondary: '#64748b',
+        dark: '#0f172a'
+      }
+    },
+  },
+  plugins: [],
+}
